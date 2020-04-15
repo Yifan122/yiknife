c80f0a8b8f5bf10cf0a7b6822cd08ea935916720
Yifan Zeng <zengyifan122@gmail.com>
2020-04-15 22:17:52 +0800

    Add annotation demo

--- demos/src/main/java/reflect/TargetClass.java
+++ demos/src/main/java/reflect/TargetClass.java
@@ -1,4 +1,18 @@
 package reflect;
 
 public class TargetClass {
+    public static void main(String[] args) throws ClassNotFoundException {
+        TargetClass targetClass = new TargetClass();
+
+        // 3 methods to get TargetClass Class object
+        Class target1 = TargetClass.class;
+
+        Class target2 = targetClass.getClass();
+
+        Class<?> target3 = Class.forName("reflect.TargetClass");
+
+        // 运行期间只有一个Class对象产生
+        System.out.println(target1 == target2);
+        System.out.println(target2 == target3 );
+    }
 }
